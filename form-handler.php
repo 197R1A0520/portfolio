2c0a06b3020d305ef5c67c9cae62605b44848af6
197R1A0520 <90521494+197R1A0520@users.noreply.github.com>
2022-02-18 11:46:16 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from ='karamthotsanthosh123@gmail.com';
+$email_subject = 'new form submission';
+$email_body = "User Name:$name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject:$subject.\n".
+              "User Message:$message.\n";
+
+$to = '197r1a0520@cmrtc.ac.in';
+$headers = "from:$email_from \r\n";
+$headers.= "Reply-to: $visitor_email\r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+?>
